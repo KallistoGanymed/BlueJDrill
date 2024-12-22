5e3cc5ae8296f10e0516621414247418d3cd59cc
Piet <obsidian42@outlook.de>
2024-12-22 14:43:28 +0100

    blue

--- BlueJ.java
+++ BlueJ.java
@@ -2,6 +2,7 @@ public class BlueJ {
     public static void main(String[]args){
         System.out.println("Hello Birdie");
         System.out.println("Hello smart IDE €");
+        //the stupid bird can now also finally use €€€ and %%% and §§§§$$$$$$$$ but the bird is still shit
     }
 
 }
